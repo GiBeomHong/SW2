5aa74070ad1ee3573f9d64a023e0d3ea6f98ebef
Gi beom Hong <ccadoguy@bitbucket.org>
2016-11-20 20:50:26 +0900

    asdfasdf

--- app.js
+++ app.js
@@ -26,12 +26,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine("html", ejs.renderFile);
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.set('host', process.env.HOST || '52.88.241.133');
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port') + " "+app.get('host'));
 });
-
 
 
 // uncomment after placing your favicon in /public
