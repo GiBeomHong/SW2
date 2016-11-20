a776c4f492dd60fe9bfb79c2c3ee91ec4f0c5950
jihwan <wlghks725@gmail.com>
2016-11-21 01:09:17 +0900

    b

--- control/pagecntr.js
+++ control/pagecntr.js
@@ -2,7 +2,7 @@
  * Created by GiBeomHong on 2016. 10. 12..
  */
 var mdd = require("../Model/Model").Model;
-
+var ip = require('my-local-ip');
 
 exports.mainpage=function (req,res,callback){
 
@@ -12,11 +12,8 @@ exports.mainpage=function (req,res,callback){
 exports.man=function (req,res,callback){
     var gender = "woman";
     mdd.find({gender:gender},function(err,model){
-        res.render("../views/man.html",{model:model});
+        res.render("../views/man.html",{model:model, host_ip:ip()});
     })
-
-    // console.log("ip is "+require('my-local-ip')());
-
 }
 
 exports.woman=function (req,res,callback){
